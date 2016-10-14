369e7abeafad2c04563923894698125a8c74398e
rj2808 <rakshit28081996@gmail.com>
2016-10-14 13:47:17 +0530

    Changing Readme

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Arduino_rng_interfacer
-Using Diode Noise we are going to generate random noise and sample it using arduino and will try to make a RNG library in MATLAB
+Using Diode Noise we are going to generate random noise and sample it using arduino and will try to make a RNG library in python
--- pyserial_learn.py
+++ pyserial_learn.py
@@ -9,21 +9,11 @@ import numpy as np
 #if data:
 #	print data #strip out the new lines for now
 		# (better to do .read() in the long run for this reason
-t = 1
-n = 0 
 A = 0
 t1 = time.time()
 while n < 30 :
-	p = 16
-	while t :
-		bytesavailable = arduino.inWaiting()
-		if bytesavailable >= p :
-			q =  int(str(arduino.read(p)), 2)/(2.0**p - 1.0)
-			arduino.flush()
-			t = 0
-			break
 	while 1 :
-		t = 1
+		q = Random(16)
 		if q != 0 :
 			A += q
 			q = 0
@@ -32,4 +22,13 @@ while n < 30 :
 t2 = time.time()
 print t2 - t1
 print A/30
+
+
+def Random(p) :
+	while 1 :
+		bytesavailable = arduino.inWaiting()
+		if bytesavailable >= p :
+			q =  int(str(arduino.read(p)), 2)/(2.0**p - 1.0)
+			arduino.flush()
+			return q
 
